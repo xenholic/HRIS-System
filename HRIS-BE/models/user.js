a2cdc4005381d93052338113af6be11f094146d0
xenholic <zenobiaholic@gmail.com>
2024-01-08 16:49:45 +0700

    adding modeling data in employee

--- HRIS-BE/models/user.js
+++ HRIS-BE/models/user.js
@@ -1,75 +1,24 @@
-import mongoose from "mongoose";
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-interface UserNotificationAttrs {
-  _userId: string;
-  // _involvedUserId: string;
-  _notificationAboutId: string;
-  notificationAbout: string;
-  notificationTitle: string;
-  notificationDesc: string;
-  notificationStatus: string;
-  sentDate: Date;
-  read: Boolean;
-}
-
-interface UserNotificationModel extends mongoose.Model<UserNotificationDoc> {
-  build(attrs: UserNotificationAttrs): UserNotificationDoc;
-}
-
-interface UserNotificationDoc extends mongoose.Document {
-  _userId: string;
-  // _involvedUserId: string;
-  _notificationAboutId: string;
-  notificationAbout: string;
-  notificationTitle: string;
-  notificationDesc: string;
-  notificationStatus: string;
-  sentDate: Date;
-  read: Boolean;
-}
-
-const userNotificationSchema = new mongoose.Schema({
-  _userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  // _involvedUserId: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: "User",
-  // },
-  _notificationAboutId: {
-    type: String,
-  },
-  notificationAbout: {
-    type: String,
-    // enum: [null, "rrlib", "action", "approved", "rejected"],
-  },
-  notificationTitle: {
-    type: String,
-  },
-  notificationDesc: {
-    type: String,
-  },
-  notificationStatus: {
-    type: String,
-    enum: [null, "deleted", "action", "approved", "rejected"],
-  },
-  sentDate: {
-    type: Date,
-    default: Date.now(),
-  },
-  read: {
-    type: Boolean,
-  },
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    roles: {
+        User: {
+            type: Number,
+            default: 2001
+        },
+        Editor: Number,
+        Admin: Number
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    refreshToken: String
 });
 
-userNotificationSchema.statics.build = (attrs: UserNotificationAttrs) => {
-  return new UserNotification(attrs);
-};
-
-const UserNotification = mongoose.model<
-  UserNotificationDoc,
-  UserNotificationModel
->("UserNotification", userNotificationSchema);
-
-export { UserNotification };
+module.exports = mongoose.model('User', userSchema);
